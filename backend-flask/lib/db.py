f3426ecf79cc23b4553a8e79251f3a706dbdfff9
imsavaliya <imsavaliya@gmail.com>
2023-04-11 18:27:13 +0000

    query working

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -6,18 +6,17 @@ import os
 connection_url = os.getenv("CONNECTION_URL")
 pool = ConnectionPool(connection_url)
 
-def query_wrap_object(template):
-  sql = '''
-  (SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (
+def query_wrap_object(self,template):
+  sql = f"""
+  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
   {template}
   ) object_row);
-  '''
+  """
   return sql
-
 def query_wrap_array(template):
-  sql = '''
+  sql = f"""
   (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
   {template}
   ) array_row);
-  '''
+  """
   return sql
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,17 +6,29 @@ class HomeActivities:
     #now = datetime.now(timezone.utc).astimezone()
     #span.set_attribute("app.now", now.isoformat())
 
-    sql = query_wrap_array("""
-    SELECT * FROM activities
-
-    """)
-
-
-    with pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql)
-        # this will return a tuple
-        # the first field being the data
-        json = cur.fetchone()
-    return json[0]  
-    return results
+    
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+      return json[0]
+      return results
